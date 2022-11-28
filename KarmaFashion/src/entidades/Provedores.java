09b3304cac4e61fc8011cdf9c89c94c51a20123e
Francisco_Melendez <francisco.melendez14454@est.uca.edu.ni>
2022-11-27 22:26:48 -0600

    Agregue el provedores

---
+++ KarmaFashion/src/entidades/Provedores.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author DELL
+ */
+public class Provedores {
+    private int provedorID;
+    private int tiendaID;
+    private String nombre;
+    private String telefono;
+    private String cedula;
+    private String email;
+    private String direccion;
+    private int sexo;
+    private int estado;
+            
+}
